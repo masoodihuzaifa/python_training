f3ac0f6848c4105490b4adb351a2c4fba9422346
masoodihuzaifa <masoodihuzaifa@gmail.com>
2017-02-19 11:03:19 +0530

    Create market_bill.py

---
+++ day_9/market_bill.py
@@ -0,0 +1,6 @@
+  print("SL.NO                    Item            Rate")
+print("####################################################################")
+print("Sl no: %d                Rice: %d        Amount: %d" % (1,5,200))
+print("Sl no: %d                Dal: %d        Amount: %d" % (2,5,1000))
+print("Sl no: %d                Pickle: %d        Amount: %d" % (3,1,100))
+~                                                                               
