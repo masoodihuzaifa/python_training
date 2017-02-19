8bc2959b9acb076eb73344e543a08361fe129401
masoodihuzaifa <masoodihuzaifa@gmail.com>
2017-02-19 13:05:46 +0530

    Update parsing.py

--- day_7/parsing.py
+++ day_7/parsing.py
@@ -46,24 +46,17 @@ class MyClass:
 		self.x=self.con.cursor(buffered=True)
 		self.query="select *from ACCESSPOINTS;"
 		self.x.execute(query)
-		#self.results =self.x.fetchall()
 		print results
-		
-
 	def parsing(self):
-		fl1=open("/home/asm/diag.out")
+		fl1=open("/home/asm/abc.out")
 		print fl1
-		
 		list_of_lines= []
 		mylist=[]
-
-
 		for line in fl1:
 		    res = re.match("----- APmgr info: apmgrinfo -a", line , re.M|re.I)
 		    if res:
 			for line in fl1:
 			    list_of_lines.append(line)
-			    #print(list)
 			    res1=re.match("65 APs are connected", line, re.M|re.I)
 		 	    if res1:
 				break
@@ -72,44 +65,31 @@ class MyClass:
 		    	if n == "\n":
 				pass
 		aps = []
-
 		ap = []
 		for line in list_of_lines:
-	
 			if line != "\n":
 				ap.append(line)
 			else:
 				ap = "".join(ap)
 				aps.append(ap)
 				ap = [] 
-		 
-		#self.x.execute("select * from ACCESSPOINTS") 
 		print("database selected")
-
 		for ap in aps:
-			
-
-		#extracting MAC  >ok
 			mac=re.search(r'([0-9a-f]{2}[:]){5}([0-9a-f]{2})',ap,re.I)
 			if mac:
 				mac=str(mac.group())
 				print mac
-		#IPV4 Extracting  >ok
 			ipv4=re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',ap,re.I)
 			if ipv4:
 				ipv4=ipv4.group()
 				print ipv4
-	
-		#IPV6 extracting >ok	
 			ipv6=re.search(r'([0-9a-f]{1,4}:){1,6}:[0-9]{1,2}',ap,re.I)
 			if ipv6:
 				ipv6=ipv6.group()
 				print ipv6
-		#Extracting Name >ok
 			name=re.search(r'(Name\s.*)',ap,re.I)
 			nameval=''	
 			if name:
-				#print name.group()
 				nmk=str(name.group())	
 				if nmk:
 					nmk1=nmk.split(':')
@@ -117,17 +97,14 @@ class MyClass:
 					print namekey		
 					nameval=nmk1[1]
 					print nameval	
-		
-		#Extracting Tunnel & Sec mode >ok
 			tse=re.search(r'(Tunnel/Sec Mode.*)',ap,re.I)
 			tunval=''
 			secval=''
 			if tse:	
-				#print tse.group()
 				tse1=str(tse.group())
 				ts1=tse1.split(':')
-				tsk=ts1[0] 	#left string key
-				tsv=ts1[1]	#right string value
+				tsk=ts1[0]
+				tsv=ts1[1]
 				tk1=tsk.split('/')
 				tunkey=tk1[0]
 				seckey=tk1[1]
@@ -138,13 +115,9 @@ class MyClass:
 				print tunval
 				print seckey
 				print secval
-		
-		
-		#extracting State Mode extracting  > ok
 			sk=re.search(r'(State.*)',ap,re.I)
 			stateval=''
 			if sk:	
-				#print sk.group()
 				skstr=str(sk.group())
 				if skstr:
 					skstr1=skstr.split(' :')
@@ -152,8 +125,6 @@ class MyClass:
 					print statekey
 					stateval=skstr1[1]			
 					print stateval
-			
-		#Mesh extracting >ok
 			mk=re.search(r'(Mesh Role.*)',ap,re.I)
 			meshval=''
 			if mk:
@@ -164,8 +135,6 @@ class MyClass:
 					print meshkey
 			 		meshval=meshstr1[1]
 					print meshval
-
-		#PSK extracting >ok
 			pk=re.search(R'(PSK)',ap,re.I)
 			pskval=''
 			if pk:
@@ -174,18 +143,15 @@ class MyClass:
 			pv=re.search(r'(PSK(.*):(.*))',ap,re.I)
 			if pv:	
 				s=str(pv.group())
-				#print s
 				if s:
 					s1=s.split(': ')	
 					pskval=s1[1]
 				print pskval
-		#Timer extracting >ok
 			tmk=re.search(r'(Timer)',ap,re.I)
 			timerval=''
 			if tmk:
 				timerkey=str(tmk.group())
 				print timerkey
-	
 			tv1=re.search(r'(Timer.*)',ap,re.I)
 			if tv1:
 				t=str(tv1.group())
@@ -193,68 +159,46 @@ class MyClass:
 					t1=t.split(': ')
 					timerval=t1[1]
 				print timerval
-		#HW Version extracting  >ok
 			hk=re.search(r'(HW)',ap,re.I)	
 			hardval=''
 			if hk:
 				hardkey=str(hk.group())
 				print hardkey
-	
 			hv=re.search(r'(\s[0-9]{1,2}.[0-9]{1}.[0-9]{1}.[0-9]{1}\s)',ap,re.I)
-	
 			if hv:
 				hardval=str(hv.group())
 				print hardval
-		#SW version Extracting >ok
-
 			sk=re.search(r'(SW)',ap,re.I)
 			softval=''
 			if sk:
 				softkey=str(sk.group())
 				print softkey
-	
-
 			sv=re.search(r'(HW.*)',ap,re.I)
 			if sv:
 				s=str(sv.group())
-				#print s
 				s1=s.split('/ ')
 				softval=s1[1]
 				print softval
-		#model & serial >ok
 			mdv=re.search(r'(Model.*)',ap,re.I)
 			modelval=''
 			serialval=''
 			if mdv:	
-				#print mdv.group()
 				s=str(mdv.group())
 				ms1=s.split(': ')
-				mskey=ms1[0]  #left string keys
-				#print mskey
+				mskey=ms1[0]
 				mskey1=mskey.split('/')
 				modelkey=mskey1[0]	
-				#print mk
 				serialkey=mskey1[1]
-				#print sk
-		
-				mvkey=ms1[1]  ##right string value
-				#print mvkey		
+				mvkey=ms1[1]		
 				mv1=mvkey.split('/')
-				modelval=mv1[0]	#msval=ms1[1]	
-				#print mv
+				modelval=mv1[0]	
 				serialval=mv1[1]
-				#print sv
-		
 				print modelkey
 				print modelval
 				print serialkey
 				print serialval
-		
-
 			print mac,ipv4,ipv6,nameval,stateval,tunval,secval,meshval,pskval,timerval,hardval,softval,modelval,serialval	
 			self.x.execute("insert into ACCESSPOINTS(MACAddress,IPv4Address,IPv6Address,Name,State,Tunnel,Sec_Mode,Mesh_Role,PSK,Timer,HW_Version,SW_Version,Model,Serial_number) values('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');" % (mac,ipv4,ipv6,nameval,stateval,tunval,secval,meshval,pskval,timerval,hardval,softval,modelval,serialval) )			
 			self.con.commit()
-			
-	
 a=MyClass()
 a.parsing()
